108f03d69429d42335c7be49dd75f0cf48e6a1ea
Maria Andreeva <mariaandreeva@MacBook-Air-Maria.local>
2022-03-30 15:34:38 +0400

    correct mistakes

--- js/form.js
+++ js/form.js
@@ -2,39 +2,56 @@
 //Активное состояние
 // eslint-disable-next-line no-unused-vars
 const toActiveState = () => {
-  const disabledForms = document.querySelectorAll('.ad-form--disabled');
-  const disabledFields = document.querySelectorAll(':disabled');
+  const formElement = document.querySelector('.ad-form');
+  formElement.classList.remove('ad-form--disabled');
 
-  disabledForms.forEach((element) => {
-    element.classList.remove('ad-form--disabled');
+  const fieldElements = document.querySelectorAll('.ad-form__element');
+  fieldElements.forEach((element) => {
+    element.removeAttribute('disabled');
   });
 
-  disabledFields.forEach((element) => {
+  const headerElement = document.querySelector('.ad-form-header');
+  headerElement.removeAttribute('disabled');
+
+  const mapFiltersElement = document.querySelector('.map__filters');
+  mapFiltersElement.classList.remove('ad-form--disabled');
+
+  const filterElements = document.querySelectorAll('.map__filter');
+  filterElements.forEach((element) => {
+    element.removeAttribute('disabled');
+  });
+
+  const featuresElements = document.querySelectorAll('.map__features');
+  featuresElements.forEach((element) => {
     element.removeAttribute('disabled');
   });
 };
 
 //Неактивное состояние
-const formElement = document.querySelector('.ad-form');
-formElement.classList.add('ad-form--disabled');
+// eslint-disable-next-line no-unused-vars
+const toBlockedState = () => {
+  const formElement = document.querySelector('.ad-form');
+  formElement.classList.add('ad-form--disabled');
 
-const fieldElements = document.querySelectorAll('.ad-form__element');
-fieldElements.forEach((element) => {
-  element.setAttribute('disabled', 'disabled');
-});
+  const fieldElements = document.querySelectorAll('.ad-form__element');
+  fieldElements.forEach((element) => {
+    element.setAttribute('disabled', 'disabled');
+  });
 
-const headerElement = document.querySelector('.ad-form-header');
-headerElement.setAttribute('disabled', 'disabled');
+  const headerElement = document.querySelector('.ad-form-header');
+  headerElement.setAttribute('disabled', 'disabled');
 
-const mapFiltersElement = document.querySelector('.map__filters');
-mapFiltersElement.classList.add('ad-form--disabled');
+  const mapFiltersElement = document.querySelector('.map__filters');
+  mapFiltersElement.classList.add('ad-form--disabled');
 
-const filterElements = document.querySelectorAll('.map__filter');
-filterElements.forEach((element) => {
-  element.setAttribute('disabled', 'disabled');
-});
+  const filterElements = document.querySelectorAll('.map__filter');
+  filterElements.forEach((element) => {
+    element.setAttribute('disabled', 'disabled');
+  });
 
-const featuresElements = document.querySelectorAll('.map__features');
-featuresElements.forEach((element) => {
-  element.setAttribute('disabled', 'disabled');
-});
+  const featuresElements = document.querySelectorAll('.map__features');
+  featuresElements.forEach((element) => {
+    element.setAttribute('disabled', 'disabled');
+  });
+};
+
