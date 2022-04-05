37ce2955833afb159af0ac5c6c7dafea51a2b2cc
Maria Andreeva <mariaandreeva@MacBook-Air-Maria.local>
2022-04-05 19:51:26 +0400

    слайдер

--- js/advertisement-form.js
+++ js/advertisement-form.js
@@ -92,3 +92,31 @@ typeField.addEventListener('change', () => {
   pristine.validate(priceField);
 });
 
+//слайдер для цены
+const sliderElement = document.querySelector('.ad-form__slider');
+const valueElement = document.querySelector('#price');
+
+valueElement.value = 5000;
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100000,
+  },
+  start: 5000,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      return value;
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+sliderElement.noUiSlider.on('update', () => {
+  valueElement.value = sliderElement.noUiSlider.get();
+});
+
