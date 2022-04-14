c6be3c46814d50ce0f245d10990b40b18b9a47db
Maria Andreeva <mariaandreeva@MacBook-Air-Maria.local>
2022-04-14 13:28:51 +0400

    fixes mixtakes

--- js/avatar-photo.js
+++ js/avatar-photo.js
@@ -1,27 +1,29 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-const avatarChooser = document.querySelector('.ad-form__field input[type=file]');
+const avatarChooser = document.querySelector('.ad-form__field input');
 const avatarPreview = document.querySelector('.ad-form-header__preview img');
 
-const photoChooser = document.querySelector('.ad-form__upload input[type=file]');
+const photoChooser = document.querySelector('.ad-form__upload input');
 const photoPreview = document.querySelector('.ad-form__photo');
 
-const onChange = (element, target, isBackground) => {
-  const file = element.files[0];
+avatarChooser.addEventListener('change', () => {
+  const file = avatarChooser.files[0];
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
-    if (isBackground) {
-      target.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
-      target.style.backgroundRepeat = 'no-repeat';
-      target.style.backgroundSize = 'contain';
-    } else {
-      target.src = URL.createObjectURL(file);
-    }
+    avatarPreview.src = URL.createObjectURL(file);
   }
-};
+});
 
-avatarChooser.addEventListener('change', () => onChange(avatarChooser, avatarPreview));
-photoChooser.addEventListener('change', () => onChange(photoChooser, photoPreview, true));
+photoChooser.addEventListener('change', () => {
+  const file = photoChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    photoPreview.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+  }
+});
